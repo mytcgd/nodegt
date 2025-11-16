eaf6e30a0f03d419625f1ed81017b1aa9d8806f7
mytcgd <131883869+mytcgd@users.noreply.github.com>
2025-11-16 14:21:44 +0800

    Add files via upload

--- app.js
+++ app.js
@@ -1,26 +1,43 @@
 const port = process.env.PORT || 3000;
+const FILE_PATH = process.env.FILE_PATH || '/tmp';
+const UUID = process.env.UUID;
 const http = require('http');
+const fs = require("fs");
+const path = require("path");
 const { spawn } = require('child_process');
-
-const server = http.createServer((req, res) => {
-    if (req.url === '/') {
-        res.writeHead(200);
-        res.end('hello world');
-    } else if (req.url === '/healthcheck') {
-        res.writeHead(200);
-        res.end('ok');
-    } else {
-        res.writeHead(404);
-        res.end('Not found');
-    }
-});
+const OPENHTTP = process.env.OPENHTTP || '0';
 
 const startScriptPath = `./start.sh`;
-const childProcess = spawn(startScriptPath, [], {
-    detached: false,
-    stdio: 'inherit',
+const startScript = spawn(startScriptPath, [], {
+    env: {
+        ...process.env,
+        OPENHTTP: OPENHTTP
+    }
+});
+startScript.stdout.on('data', (data) => {
+    console.log(`${data}`);
+});
+startScript.stderr.on('data', (data) => {
+    console.error(`${data}`);
+});
+startScript.on('error', (error) => {
+    console.error(`boot error: ${error}`);
+    process.exit(1);
 });
 
-server.listen(port, () => {
+if (OPENHTTP === '1') {
+    const server = http.createServer((req, res) => {
+        if (req.url === '/') {
+            res.writeHead(200);
+            res.end('hello world');
+        } else {
+            res.writeHead(404);
+            res.end('Not found');
+        }
+    });
+    server.listen(port, () => {
+        console.log(`server is listening on port ${port}`);
+    });
+} else if (OPENHTTP === '0') {
     console.log(`server is listening on port ${port}`);
-});
+}
